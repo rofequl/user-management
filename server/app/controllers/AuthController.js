ff016b78d918b778c2a2922732cf56ee3edd71ed
Nayem <rofequlislamnayem@gmail.com>
2024-03-23 22:10:35 +0600

    Express Validate Add

--- server/app/controllers/AuthController.js
+++ server/app/controllers/AuthController.js
@@ -1,4 +1,20 @@
+const {body, validationResult} = require("express-validator");
+
 module.exports.login = [
+    body("email", "User email is required!").notEmpty().custom((value) => {
+        return /^.+@(?:[\w-]+\.)+\w+$/.test(value) ? Promise.resolve() : Promise.reject("Invalid email address")
+    }),
+    body("password", "User password required!").notEmpty().isLength({min: 6, max: 20})
+        .withMessage("password length minimum 6 & maximum 20!"),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) return res.status(422).json({success: false, errors: errors.mapped()});
+
+        res.send('Hello World!')
+    }
+]
+
+module.exports.register = [
     async (req, res) => {
         res.send('Hello World!')
     }
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,5 +3,6 @@ const router = Router()
 const AuthController = require('../app/controllers/AuthController')
 
 router.post("/user/login", AuthController.login)
+router.post("/user/register", AuthController.register)
 
 module.exports = router;
